5344423d02a23394640ca5b0ca9be40004f777c2
NazarethR <nazareth2006ramirez@gmail.com>
2025-07-28 19:23:27 -0600

    se crea la parte logica de Cinturones
    Se crea la clase cinturones y asientos

---
+++ src/Cinturones/Asientos.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cinturones;
+
+/**
+ *
+ * @author nazar
+ */
+public class Asientos {
+    private Cinturon Conductor;
+    private Cinturon Copiloto;
+    private Cinturon Pasajero1;
+    private Cinturon Pasagero2;
+
+    public Cinturon getConductor() {
+        return Conductor;
+    }
+
+    public void setConductor(Cinturon Conductor) {
+        this.Conductor = Conductor;
+    }
+
+    public Cinturon getCopiloto() {
+        return Copiloto;
+    }
+
+    public void setCopiloto(Cinturon Copiloto) {
+        this.Copiloto = Copiloto;
+    }
+
+    public Cinturon getPasajero1() {
+        return Pasajero1;
+    }
+
+    public void setPasajero1(Cinturon Pasajero1) {
+        this.Pasajero1 = Pasajero1;
+    }
+
+    public Cinturon getPasagero2() {
+        return Pasagero2;
+    }
+
+    public void setPasagero2(Cinturon Pasagero2) {
+        this.Pasagero2 = Pasagero2;
+    }
+
+    public Asientos(Cinturon Conductor, Cinturon Copiloto, Cinturon Pasajero1, Cinturon Pasagero2) {
+        this.Conductor = Conductor;
+        this.Copiloto = Copiloto;
+        this.Pasajero1 = Pasajero1;
+        this.Pasagero2 = Pasagero2;
+    }
+
+   public void abrochar(Cinturon e){
+       e.setEstado( true);
+   }
+     public void desabrochar(Cinturon e){
+       e.setEstado( false);
+   }
+    
+}
---
+++ src/Cinturones/Cinturon.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cinturones;
+
+/**
+ *
+ * @author nazar
+ */
+public class Cinturon {
+
+    private boolean Estado;
+
+    public boolean isEstado() {
+        return Estado;
+    }
+
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    }
+
+    public Cinturon() {
+        this.Estado = false;
+    }
+
+}
