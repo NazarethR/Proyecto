3783d35cfacdd30221b1a2aaf6fc421cec4a3733
NazarethR <nazareth2006ramirez@gmail.com>
2025-07-30 11:09:36 -0600

    Se modifica la clase palabrisas
    Modifique la clase palabrisas para que solo funcione si el motor esta encendido

--- src/Parabrisas/Palabrisas.java
+++ src/Parabrisas/Palabrisas.java
@@ -8,32 +8,40 @@ package Parabrisas;
  *
  * @author nazar
  */
-public class Palabrisas {
+public class Palabrisas  {
+  private SistemaEncendido motor; // referencia al sistema de encendido
+    private int velocidad; // 0 = apagado, 1-3 = niveles de velocidad
 
-    private int velocidad;
+    public Palabrisas(SistemaEncendido motor) {
+        this.motor = motor;
+        this.velocidad = 0; // apagado por defecto
+    }
 
     public int getVelocidad() {
         return velocidad;
     }
 
     public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
+        // Solo cambia la velocidad si el motor está encendido
+        if (motor.estaEncendido()) {
+            this.velocidad = velocidad;
+        } else {
+            this.velocidad = 0; // forzar apagado si el motor no está encendido
+        }
     }
 
-    public Palabrisas(int velocidad) {
-        this.velocidad = velocidad;
-    }
+    public String obtenerEstado() {
+        if (!motor.estaEncendido()) {
+            return "Motor apagado. Palabrisas desactivado.";
+        }
 
-    public String getEstado() {
         return switch (velocidad) {
-            case 1 ->
-                "Velocidad lenta";
-            case 2 ->
-                "Velocidad media";
-            case 3 ->
-                "Velocidad rápida";
-            default ->
-                "Apagado";
+            case 1 -> "Velocidad lenta";
+            case 2 -> "Velocidad media";
+            case 3 -> "Velocidad rápida";
+            default -> "Palabrisas apagado";
         };
     }
+
+    
 }
